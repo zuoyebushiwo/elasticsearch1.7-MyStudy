bf3d9d89f93730ae157d89e1aa31a3f87edac071
zhangxuej <957168943@qq.com>
2015-07-24 08:33:26 +0800

    Change-Id: I2dcbae95c652f82b5a793957c14202fe628d353b

---
+++ src/main/java/jsr166e/Striped64.java
@@ -0,0 +1,37 @@
+/*
+ * Written by Doug Lea with assistance from members of JCP JSR-166
+ * Expert Group and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package jsr166e;
+import java.util.Random;
+
+/**
+ * A package-local class holding common representation and mechanics
+ * for classes supporting dynamic striping on 64bit values. The class
+ * extends Number so that concrete subclasses must publicly do so.
+ */
+class Striped64 extends Number  {
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+
+}
